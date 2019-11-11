1893a0c7ce518575f843e4df96bf12b5de947584
joshtom <olajidejoshua4real@gmail.com>
2019-11-11 11:52:25 +0100

    Modified app.js file

---
+++ networking(fetch)/app.js
@@ -0,0 +1,31 @@
+/*
+ *
+ *
+    Using fetch with Headers
+ * 
+ * 
+ */
+// fetch('https://api.github.com/users?since=135').then(function(response) {
+//   console.log(response.headers.get('Content-Type'));
+//   console.log(response.headers.get('Date'));
+
+//   console.log(response.status);
+//   console.log(response.statusText);
+//   console.log(response.type);
+//   console.log(response.url);
+// });
+
+/* Using fetch with responses types
+ 
+fetch('https://api.github.com/users', {mode: 'cors'})
+  .then(function(response) {
+    return response.text();
+  })
+  .then(function(text) {
+    console.log('Request successful', text);
+  })
+  .catch(function(error) {
+    console.log('Request failed', error)
+  });
+ * 
+ */
