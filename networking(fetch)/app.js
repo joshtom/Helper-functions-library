f42e705df21e95f03cf979c46ca650712b2d5987
joshtom <olajidejoshua4real@gmail.com>
2019-11-11 16:43:34 +0100

    Exported all the functions

--- networking(fetch)/app.js
+++ networking(fetch)/app.js
@@ -22,48 +22,59 @@ const json = response => { return response.json() };
  * 
  * 
  */
-// fetch('https://api.github.com/users?since=135').then(function(response) {
-//   console.log(response.headers.get('Content-Type'));
-//   console.log(response.headers.get('Date'));
+export function fetchWithHeader() {
+    fetch('https://api.github.com/users?since=135').then(function(response) {
+    console.log(response.headers.get('Content-Type'));
+    console.log(response.headers.get('Date'));
 
-//   console.log(response.status);
-//   console.log(response.statusText);
-//   console.log(response.type);
-//   console.log(response.url);
-// });
+    console.log(response.status);
+    console.log(response.statusText);
+    console.log(response.type);
+    console.log(response.url);
+    });
+}
 
 /* Using fetch with responses types
- 
-fetch('https://api.github.com/users', {mode: 'cors'})
-  .then(function(response) {
-    return response.text();
-  })
-  .then(function(text) {
-    console.log('Request successful', text);
-  })
-  .catch(function(error) {
-    console.log('Request failed', error)
-  });
- * 
+* 
  */
+ 
+ export function fetchWithResponseType() {
+    fetch('https://api.github.com/users', {mode: 'cors'})
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(text) {
+      console.log('Request successful', text);
+    })
+    .catch(function(error) {
+      console.log('Request failed', error)
+    });
+ }
+ 
 
 /** Combining the fetch function with the function declared above
  * 
- * fetch('https://api.github.com/users/joshtom')
-.then(status)
-.then(json)
-.then((data) => {
-    console.log('Request succeeded with json response', data);
-})
-.catch((err) => {
-    console.log('Request failed', err);
-})
+ * 
  */
+export function normalFetch() {
+    fetch('https://api.github.com/users/joshtom')
+    .then(status)
+    .then(json)
+    .then((data) => {
+        console.log('Request succeeded with json response', data);
+    })
+    .catch((err) => {
+        console.log('Request failed', err);
+    })
+}
+ 
 
 /**
  * POST request with a Fetch Request
  * 
- * fetch("https://api.github.com/users/joshtom", {
+ *  */
+export function postFetch() {
+    fetch("https://api.github.com/users/joshtom", {
     method: 'POST',
     headers: {
         "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
@@ -76,62 +87,70 @@ fetch('https://api.github.com/users', {mode: 'cors'})
 }).then((err) => {
     console.log("Error fetching", err);
 })
- */
+
+}
 
  /**
   * FUNCTION THAT RETURN GITHUB USERS GISTS
   */
 
-//   async function getUsers(names) {
-//     let jobs = [];
-
-//     for (let name of names) {
-//         let job = fetch(`https://api.github.com/users/${name}`)
-//         .then(status)
-//         .then(json)
-//         .catch((err) => {console.log("Error", err) })
-//         jobs.push(job);
-//     }
-//     let results = await Promise.all(jobs);
-
-//     return results;
-
-//   }
+  export function fetchAsync() {
+    async function getUsers(names) {
+        let jobs = [];
+    
+        for (let name of names) {
+            let job = fetch(`https://api.github.com/users/${name}`)
+            .then(status)
+            .then(json)
+            .catch((err) => {console.log("Error", err) })
+            jobs.push(job);
+        }
+        let results = await Promise.all(jobs);
+    
+        return results;
+    
+      }
+  }
 
 /**
  * FETCHING USING ASYNC AND AWAIT TO FETCH DATA ASYNCHRONOUSLY
- *  async function getUser (names) {
-    let jobs = [];
-    for(name of names) {
-        // let job = fetch(`https://api.github.com/users/${name}`)
-        let job = fetch(`https://thesimpsonsquoteapi.glitch.me/${name}`)
-        .then(status)
-        .then(json)
-        .then(data => {
-            console.log("Data retrieved", data);
-        }).catch(err => {
-            console.log(err);
-        })
-        let push = jobs.push(job)
-        // console.log(push); Print the number of arrays passed into the function
+ * */
+export function asyncAwait() {
+    async function getUser (names) {
+        let jobs = [];
+        for(name of names) {
+            // let job = fetch(`https://api.github.com/users/${name}`)
+            let job = fetch(`https://thesimpsonsquoteapi.glitch.me/${name}`)
+            .then(status)
+            .then(json)
+            .then(data => {
+                console.log("Data retrieved", data);
+            }).catch(err => {
+                console.log(err);
+            })
+            let push = jobs.push(job)
+            // console.log(push); Print the number of arrays passed into the function
+        }
+        let results = await Promise.all(jobs);
+        return results;
+        
+        
     }
-    let results = await Promise.all(jobs);
-    return results;
-    
-    
 }
-let name = ["quotes"];
-getUser(name);
+// let name = ["quotes"];
+// getUser(name);
 
- */
+ 
 
  /**
   * Sending Credentials with a Fetch Request
   * Credentials such as Cookies
   */
- fetch(url, {
-     credentials: `inlude`
- })
+export function fetchCredentials() {
+    fetch(url, {
+        credentials: `include`
+    })
+}
 
   
 
